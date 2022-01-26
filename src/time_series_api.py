e9e9392ee349963c185e871a668ee372f554402b
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-01-26 16:45:01 +0900

    error handling for pearson calc

--- src/time_series_api.py
+++ src/time_series_api.py
@@ -67,12 +67,15 @@ class TimeSeriesAPI:
                 log.warning("Prediction DataFrame contains only one investment_id.")
                 return
 
-            # r: ピアソン相関係数 -1 <= r <= 1 の値を取り、1に近いほど正の相関、-1に近いほど負の相関、0に近いほど無相関
-            # prob: 相関が優位かどうかを示す。例えば、 p = 0.05 なら実際には相関がないにも関わらず、相関が存在すると
-            #       判定されるリスクが 5% ある。概ね、 p <= 0.05 なら相関が 0 とは異なる（優位である）と結論付けられる。
-            r, prob = ss.pearsonr(pred_df["target"].values, self.targets[-1]["target"].values)
-            # log.debug(f"pred_df: {pred_df['target'].values}")
+            try:
+                # r: ピアソン相関係数 -1 <= r <= 1 の値を取り、1に近いほど正の相関、-1に近いほど負の相関、0に近いほど無相関
+                # prob: 相関が優位かどうかを示す。例えば、 p = 0.05 なら実際には相関がないにも関わらず、相関が存在すると
+                #       判定されるリスクが 5% ある。概ね、 p <= 0.05 なら相関が 0 とは異なる（優位である）と結論付けられる。
+                r, prob = ss.pearsonr(pred_df["target"].values, self.targets[-1]["target"].values)
+                # log.debug(f"pred_df: {pred_df['target'].values}")
 
-            self.score.update(r)
-            self.rs.append(r)
-            self.probs.append(prob)
+                self.score.update(r)
+                self.rs.append(r)
+                self.probs.append(prob)
+            except ValueError as e:
+                log.warning(f"TimeSeriesAPI.predict: {e}")
