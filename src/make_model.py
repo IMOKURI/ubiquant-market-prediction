1146e054139106c27d5b39e9083805a77d306365
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-02-01 22:30:23 +0900

    add 1DCNN model

--- src/make_model.py
+++ src/make_model.py
@@ -11,7 +11,9 @@ log = logging.getLogger(__name__)
 
 def make_model(c, device=None, model_path=None):
     if c.params.model == "ump_1":
-        model = BaseModel(c, model_path)
+        model = MLPModel(c)
+    elif c.params.model == "ump_1dcnn":
+        model = OneDCNNModel(c)
     else:
         raise Exception("Invalid model.")
 
@@ -39,8 +41,8 @@ def swish(x):
     return x * torch.sigmoid(x)
 
 
-class BaseModel(nn.Module):
-    def __init__(self, c, pretrained=True):
+class MLPModel(nn.Module):
+    def __init__(self, c):
         super().__init__()
         self.amp = c.settings.amp
         self.dim = 300
@@ -57,6 +59,82 @@ class BaseModel(nn.Module):
             x = self.bn_1(x)
             for fc in self.fcs:
                 x = swish(fc(x))
+
+            x = self.head(x)
+
+        return x
+
+
+# https://www.kaggle.com/c/lish-moa/discussion/202256
+# https://github.com/baosenguo/Kaggle-MoA-2nd-Place-Solution/blob/main/training/1d-cnn-train.ipynb
+class OneDCNNModel(nn.Module):
+    def __init__(self, c):
+        super().__init__()
+        self.amp = c.settings.amp
+        self.input = 300
+
+        self.hidden_size = 1024
+        self.ch_1 = 128
+        self.ch_2 = 384
+        self.ch_3 = 384
+
+        self.ch_po_1 = int(self.hidden_size / self.ch_1 / 2)
+        self.ch_po_2 = int(self.hidden_size / self.ch_1 / 2 / 2) * self.ch_3
+
+        self.expand = nn.Sequential(
+            nn.BatchNorm1d(self.input), nn.utils.weight_norm(nn.Linear(self.input, self.hidden_size)), nn.CELU(0.06)
+        )
+
+        self.conv1 = nn.Sequential(
+            nn.BatchNorm1d(self.ch_1),
+            nn.Dropout(0.1),
+            nn.utils.weight_norm(
+                nn.Conv1d(self.ch_1, self.ch_2, kernel_size=5, stride=1, padding=2, bias=False), dim=None
+            ),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool1d(output_size=self.ch_po_1),
+            nn.BatchNorm1d(self.ch_2),
+            nn.Dropout(0.1),
+            nn.utils.weight_norm(
+                nn.Conv1d(self.ch_2, self.ch_2, kernel_size=3, stride=1, padding=1, bias=True), dim=None
+            ),
+            nn.ReLU(),
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.BatchNorm1d(self.ch_2),
+            nn.Dropout(0.3),
+            nn.utils.weight_norm(
+                nn.Conv1d(self.ch_2, self.ch_2, kernel_size=3, stride=1, padding=1, bias=True), dim=None
+            ),
+            nn.ReLU(),
+            nn.BatchNorm1d(self.ch_2),
+            nn.Dropout(0.2),
+            nn.utils.weight_norm(
+                nn.Conv1d(self.ch_2, self.ch_3, kernel_size=5, stride=1, padding=2, bias=True), dim=None
+            ),
+            nn.ReLU(),
+        )
+
+        self.max_po_c2 = nn.MaxPool1d(kernel_size=4, stride=2, padding=1)
+
+        self.flt = nn.Flatten()
+
+        self.head = nn.Sequential(
+            nn.BatchNorm1d(self.ch_po_2),
+            nn.Dropout(0.2),
+            nn.utils.weight_norm(nn.Linear(self.ch_po_2, 1)),
+        )
+
+    def forward(self, x):
+        with amp.autocast(enabled=self.amp):
+            x = self.expand(x).view(x.size(0), self.ch_1, -1)
+
+            x = self.conv1(x)
+            x = self.conv2(x) * x
+
+            x = self.max_po_c2(x)
+            x = self.flt(x)
 
             x = self.head(x)
 
