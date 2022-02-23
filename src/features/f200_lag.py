784daaf7652af6280789233af82838097a28d1be
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-02-23 21:11:19 +0900

    add lag features

--- src/features/f200_lag.py
+++ src/features/f200_lag.py
@@ -7,6 +7,20 @@ from .base import Context, feature
 
 @feature([f"lag_last_f_{n}" for n in range(300)])
 def f200_lag_last_features(ctx: Context) -> Dict[str, float]:
-    last_2 = ctx.store.investments[ctx.investment_id].features.last_n(2)
-    last_2_lag = last_2[-1] - last_2[-2]
-    return {f"lag_last_f_{n}": v for n, v in enumerate(last_2_lag.squeeze())}
+    last_n = ctx.store.investments[ctx.investment_id].features.last_n(2)
+    last_n_lag = last_n[-1] - last_n[0]
+    return {f"lag_last_f_{n}": v for n, v in enumerate(last_n_lag.squeeze())}
+
+
+@feature([f"lag_last_3_f_{n}" for n in range(300)])
+def f201_lag_last_3_features(ctx: Context) -> Dict[str, float]:
+    last_n = ctx.store.investments[ctx.investment_id].features.last_n(3)
+    last_n_lag = last_n[-1] - last_n[0]
+    return {f"lag_last_3_f_{n}": v for n, v in enumerate(last_n_lag.squeeze())}
+
+
+@feature([f"lag_last_4_f_{n}" for n in range(300)])
+def f202_lag_last_4_features(ctx: Context) -> Dict[str, float]:
+    last_n = ctx.store.investments[ctx.investment_id].features.last_n(4)
+    last_n_lag = last_n[-1] - last_n[0]
+    return {f"lag_last_4_f_{n}": v for n, v in enumerate(last_n_lag.squeeze())}
