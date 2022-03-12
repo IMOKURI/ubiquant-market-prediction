a266eb6ab54f64222355d04b5b639ce2c14e3b4d
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-03-12 21:57:15 +0900

    1dcnn

--- src/make_model.py
+++ src/make_model.py
@@ -188,10 +188,10 @@ class OneDCNNModel(nn.Module):
         self.amp = c.settings.amp
         self.input = c.params.model_input
 
-        self.hidden_size = 4096
-        self.ch_1 = 256
-        self.ch_2 = 512
-        self.ch_3 = 512
+        self.hidden_size = 1024
+        self.ch_1 = 128
+        self.ch_2 = 384
+        self.ch_3 = 384
 
         self.ch_po_1 = int(self.hidden_size / self.ch_1 / 2)
         self.ch_po_2 = int(self.hidden_size / self.ch_1 / 2 / 2) * self.ch_3
