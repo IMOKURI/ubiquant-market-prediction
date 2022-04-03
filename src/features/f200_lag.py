326edad378eb39d5dd79f8367142ffa6a2a4f5c6
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-04-03 20:48:27 +0900

    lag feature for lstm

--- src/features/f200_lag.py
+++ src/features/f200_lag.py
@@ -22,3 +22,13 @@ def f202_lag_last_4_features(ctx: Context) -> Dict[str, float]:
     last_n = ctx.store.investments[ctx.investment_id].features.last_n(4)
     last_n_lag = last_n[-1] - last_n[0]
     return {f"lag_last_4_f_{n}": v for n, v in enumerate(last_n_lag.squeeze())}
+
+
+@feature([f"lag_last_f_{time_id}_{n}" for time_id in range(10) for n in range(300)])
+def f210_lag_last_features_last10(ctx: Context) -> Dict[str, float]:
+    last_11 = ctx.store.investments[ctx.investment_id].features.last_n(11)
+    features = {}
+    for time_id, (row_last, row_current) in enumerate(zip(last_11[:-1], last_11[1:])):
+        for n, (val_last, val_current) in enumerate(zip(row_last, row_current)):
+            features[f"lag_last_f_{time_id - 1}_{n}"] = val_current - val_last
+    return features
